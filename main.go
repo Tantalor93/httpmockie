e8ef13bafe80f64e0655dd169832cd1e31d55d89
Ondrej Benkovsky <ondrej.benkovsky@wandera.com>
2020-07-13 15:11:42 +0200

    tiger focused mock

--- main.go
+++ main.go
@@ -3,58 +3,26 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"github.com/gorilla/mux"
-	"go-http-mock/internal/log"
-	"go-http-mock/internal/mockspec"
-
 	"net/http"
 	"strconv"
 )
 
-var router = mux.NewRouter()
-
-func main() {
-	defer log.Logger.Sync()
-
-	port, dir := parseFlags()
-
-	specs := mockspec.CollectFromDirectory(*dir)
-
-	for _, spec := range specs {
-		log.Logger.Infof("Registering handler for path '%s'", spec.Path)
-		createHandler(spec)
-	}
-
-	itoa := strconv.Itoa(*port)
-	if err := http.ListenAndServe(":"+itoa, router); err != nil {
-		panic(err)
-	}
+type SomeResponse struct {
+	Msg string `json:"msg"`
 }
 
-func parseFlags() (*int, *string) {
+func main() {
 	port := flag.Int("port", 8081, "port")
-	dir := flag.String("dir", ".", "directory with mock specification")
 
 	flag.Parse()
 
-	if port == nil || dir == nil {
-		log.Logger.Errorf("--port and --dir flags have to be specified")
-		panic("Required arguments not provided")
-	}
 
-	return port, dir
-}
-
-func createHandler(mock mockspec.MockSpecification) *mux.Route {
-	return router.HandleFunc(mock.Path, func(writer http.ResponseWriter, request *http.Request) {
-		if mock.Status != 0 {
-			writer.WriteHeader(mock.Status)
-		}
-		if mock.JsonBody != nil {
-			json.NewEncoder(writer).Encode(mock.JsonBody)
-		}
-		if mock.Body != nil {
-			writer.Write([]byte(*mock.Body))
-		}
+	http.HandleFunc("/mock", func(writer http.ResponseWriter, request *http.Request) {
+		response := SomeResponse{Msg: "Benky jede"}
+		writer.WriteHeader(http.StatusOK)
+		json.NewEncoder(writer).Encode(response)
 	})
+
+	itoa := strconv.Itoa(*port)
+	http.ListenAndServe(":"+itoa, nil)
 }
